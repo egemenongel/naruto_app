a7ad62dea622747a8123bea078c847b7e0f4f03b
egemenongel <egemenongel@hotmail.com>
2024-05-24 18:32:39 +0300

    Initial Commit

---
+++ NarutoApp/NarutoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NarutoAppApp.swift
+//  NarutoApp
+//
+//  Created by Egemen Öngel on 24.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct NarutoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
